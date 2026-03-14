fc07d5ce882efb2f865d62bc7d19cf40d7151401
nethmalgeesaraux <nethmalpaypal@gmail.com>
2026-03-14 12:24:27 +0530

    feat: initial commit

--- app/page.js
+++ app/page.js
@@ -1,10 +1,11 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       <h1>Wellcome</h1>
-      
+      <Button variant="destructive">Click Me</Button>
     </div>
   );
 }
