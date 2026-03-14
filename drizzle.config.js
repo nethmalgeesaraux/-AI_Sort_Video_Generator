dfafa031c28e2064f9ee22a8859b9e3fd61cb94a
nethmalgeesaraux <nethmalpaypal@gmail.com>
2026-03-14 21:58:39 +0530

    Pulling db

--- drizzle.config.js
+++ drizzle.config.js
@@ -3,6 +3,6 @@ export default {
   schema: "./configs/schema.js",   
   dialect: "postgresql",           
   dbCredentials: {
-    url:' postgresql://neondb_owner:npg_VNRQ51SOtPhT@ep-summer-cloud-a4m8hnc1-pooler.us-east-1.aws.neon.tech/ai_shot_video_generator?sslmode=require&channel_binding=require, '      
+  url: "postgresql://neondb_owner:npg_VNRQ51SOtPhT@ep-summer-cloud-a4m8hnc1-pooler.us-east-1.aws.neon.tech/ai_shot_video_generator?sslmode=require&channel_binding=require"     
   },
 };
